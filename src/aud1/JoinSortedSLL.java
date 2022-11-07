033f79830e54e46c21cd8b681ce4c8eed5edba78
TodorM <todormitevski5@gmail.com>
2022-11-07 16:03:28 +0100

    add aud1

---
+++ src/aud1/JoinSortedSLL.java
@@ -0,0 +1,33 @@
+package aud1;
+
+public class JoinSortedSLL<E extends Comparable<E>> {
+
+    public AudSLL<E> joinSortedLists(AudSLL<E> firstList, AudSLL<E> secondList){
+
+        AudSLL<E> result = new AudSLL<E>();
+        AudSLLNode<E> tmpFirst = firstList.getFirst();
+        AudSLLNode<E> tmpSecond = secondList.getFirst();
+
+        while(tmpFirst != null && tmpSecond != null){
+            if(tmpFirst.element.equals(tmpSecond.element)){
+                result.insertLast(tmpFirst.element);
+                tmpFirst = tmpFirst.succ;
+            } else{
+                result.insertLast(tmpSecond.element);
+                tmpSecond = tmpSecond.succ;
+            }
+        }
+
+        while(tmpFirst != null){
+            result.insertLast(tmpFirst.element);
+            tmpFirst = tmpFirst.succ;
+        }
+
+        while(tmpSecond != null){
+            result.insertLast(tmpSecond.element);
+            tmpSecond = tmpSecond.succ;
+        }
+
+        return null;
+    }
+}
