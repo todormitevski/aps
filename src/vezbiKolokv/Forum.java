9be1e4eaef2a321126eb1c9587b7dc929fef145f
TodorM <todormitevski5@gmail.com>
2022-11-30 03:24:53 +0100

    add kolokviumski

--- src/vezbiKolokv/Forum.java
+++ src/vezbiKolokv/Forum.java
@@ -1,277 +1,277 @@
-package vezbiKolokv;
-
-import java.util.Scanner;
-
-class Discussion {
-    private int id;
-    private int popularity;
-    private int users;
-
-    public Discussion(int id, int popularity, int users) {
-        this.id = id;
-        this.popularity = popularity;
-        this.users = users;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(int popularity) {
-        this.popularity = popularity;
-    }
-
-    public int getUsers() {
-        return users;
-    }
-
-    public void setUsers(int users) {
-        this.users = users;
-    }
-    //new
-    public int importance(){
-        return popularity * users;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(id);
-    }
-}
-
-class SLLNode<E> {
-    protected E element;
-    protected SLLNode<E> succ;
-
-    public SLLNode(E elem, SLLNode<E> succ) {
-        this.element = elem;
-        this.succ = succ;
-    }
-
-    @Override
-    public String toString() {
-        return element.toString();
-    }
-}
-
-class SLL<E> {
-    private SLLNode<E> first;
-
-    public SLL() {
-        this.first = null;
-    }
-
-    public void deleteList() {
-        first = null;
-    }
-
-    public int length() {
-        int ret;
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            ret = 1;
-            while (tmp.succ != null) {
-                tmp = tmp.succ;
-                ret++;
-            }
-            return ret;
-        } else
-            return 0;
-
-    }
-
-    @Override
-    public String toString() {
-        String ret = new String();
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            ret += tmp;
-            while (tmp.succ != null) {
-                tmp = tmp.succ;
-                ret += " " + tmp;
-            }
-        } else
-            ret = "Prazna lista!!!";
-        return ret;
-    }
-
-    public void insertFirst(E o) {
-        SLLNode<E> ins = new SLLNode<E>(o, first);
-        first = ins;
-    }
-
-    public void insertAfter(E o, SLLNode<E> node) {
-        if (node != null) {
-            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
-            node.succ = ins;
-        } else {
-            System.out.println("Dadenot jazol e null");
-        }
-    }
-
-    public void insertBefore(E o, SLLNode<E> before) {
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            if (first == before) {
-                this.insertFirst(o);
-                return;
-            }
-            while (tmp.succ != before)
-                tmp = tmp.succ;
-            if (tmp.succ == before) {
-                SLLNode<E> ins = new SLLNode<E>(o, before);
-                tmp.succ = ins;
-            } else {
-                System.out.println("Elementot ne postoi vo listata");
-            }
-        } else {
-            System.out.println("Listata e prazna");
-        }
-    }
-
-    public void insertLast(E o) {
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            while (tmp.succ != null)
-                tmp = tmp.succ;
-            SLLNode<E> ins = new SLLNode<E>(o, null);
-            tmp.succ = ins;
-        } else {
-            insertFirst(o);
-        }
-    }
-
-    public E deleteFirst() {
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            first = first.succ;
-            return tmp.element;
-        } else {
-            System.out.println("Listata e prazna");
-            return null;
-        }
-    }
-
-    public E delete(SLLNode<E> node) {
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            if (first == node) {
-                return this.deleteFirst();
-            }
-            while (tmp.succ != node && tmp.succ.succ != null)
-                tmp = tmp.succ;
-            if (tmp.succ == node) {
-                tmp.succ = tmp.succ.succ;
-                return node.element;
-            } else {
-                System.out.println("Elementot ne postoi vo listata");
-                return null;
-            }
-        } else {
-            System.out.println("Listata e prazna");
-            return null;
-        }
-    }
-
-    public SLLNode<E> getFirst() {
-        return first;
-    }
-
-    public SLLNode<E> find(E o) {
-        if (first != null) {
-            SLLNode<E> tmp = first;
-            while (tmp.element != o && tmp.succ != null)
-                tmp = tmp.succ;
-            if (tmp.element == o) {
-                return tmp;
-            } else {
-                System.out.println("Elementot ne postoi vo listata");
-            }
-        } else {
-            System.out.println("Listata e prazna");
-        }
-        return first;
-    }
-}
-
-class Forum {
-    // TODO: implement function
-    private static void forum(SLL<Discussion> health, SLL<Discussion> finance) {
-        //nasiot kod
-        /*
-        input:
-        3 - br health discuss  3 - br finance discuss
-        id     pop  users
-        111    1    1
-        222    2    4
-        333    1    1
-        444    2    7
-        555    4    6
-        666    2    9
-
-        importance = popularity * users
-
-        remove the least important discussion from finance
-        insert the most important discuss. from health
-        into finance, it has to have identical
-        id, pop and users..?
-        */
-
-        SLLNode<Discussion> healthDiscussions = health.getFirst();
-        SLLNode<Discussion> financeDiscussions = finance.getFirst();
-        int importance = financeDiscussions.element.importance();
-        Discussion leastImportant = financeDiscussions.element;
-
-        while(financeDiscussions != null){
-            if(financeDiscussions.element.importance() <
-                    leastImportant.importance()){
-                leastImportant = financeDiscussions.element;
-            }
-            financeDiscussions = financeDiscussions.succ;
-        }
-        finance.delete(finance.find(leastImportant));
-
-        Discussion mostImportant = healthDiscussions.element;
-        while(healthDiscussions != null){
-            if(healthDiscussions.element.importance() >
-                    mostImportant.importance()){
-                mostImportant = healthDiscussions.element;
-            }
-            healthDiscussions = healthDiscussions.succ;
-        }
-        finance.insertLast(mostImportant);
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int numHealth = Integer.parseInt(scanner.nextLine());
-        int numFinance = Integer.parseInt(scanner.nextLine());
-
-        SLL<Discussion> health = new SLL<Discussion>();
-        SLL<Discussion> finance = new SLL<Discussion>();
-
-        for (int i = 0; i < numHealth; i++) {
-            String line = scanner.nextLine();
-            String[] parts = line.split("\\s+");
-            health.insertLast(new Discussion(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
-        }
-
-        for (int i = 0; i < numFinance; i++) {
-            String line = scanner.nextLine();
-            String[] parts = line.split("\\s+");
-            finance.insertLast(new Discussion(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
-        }
-
-        forum(health, finance);
-        System.out.println(health.toString());
-        System.out.println(finance.toString());
-    }
-}
+//package vezbiKolokv;
+//
+//import java.util.Scanner;
+//
+//class Discussion {
+//    private int id;
+//    private int popularity;
+//    private int users;
+//
+//    public Discussion(int id, int popularity, int users) {
+//        this.id = id;
+//        this.popularity = popularity;
+//        this.users = users;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getPopularity() {
+//        return popularity;
+//    }
+//
+//    public void setPopularity(int popularity) {
+//        this.popularity = popularity;
+//    }
+//
+//    public int getUsers() {
+//        return users;
+//    }
+//
+//    public void setUsers(int users) {
+//        this.users = users;
+//    }
+//    //new
+//    public int importance(){
+//        return popularity * users;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return String.valueOf(id);
+//    }
+//}
+//
+//class SLLNode<E> {
+//    protected E element;
+//    protected SLLNode<E> succ;
+//
+//    public SLLNode(E elem, SLLNode<E> succ) {
+//        this.element = elem;
+//        this.succ = succ;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return element.toString();
+//    }
+//}
+//
+//class SLL<E> {
+//    private SLLNode<E> first;
+//
+//    public SLL() {
+//        this.first = null;
+//    }
+//
+//    public void deleteList() {
+//        first = null;
+//    }
+//
+//    public int length() {
+//        int ret;
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            ret = 1;
+//            while (tmp.succ != null) {
+//                tmp = tmp.succ;
+//                ret++;
+//            }
+//            return ret;
+//        } else
+//            return 0;
+//
+//    }
+//
+//    @Override
+//    public String toString() {
+//        String ret = new String();
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            ret += tmp;
+//            while (tmp.succ != null) {
+//                tmp = tmp.succ;
+//                ret += " " + tmp;
+//            }
+//        } else
+//            ret = "Prazna lista!!!";
+//        return ret;
+//    }
+//
+//    public void insertFirst(E o) {
+//        SLLNode<E> ins = new SLLNode<E>(o, first);
+//        first = ins;
+//    }
+//
+//    public void insertAfter(E o, SLLNode<E> node) {
+//        if (node != null) {
+//            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+//            node.succ = ins;
+//        } else {
+//            System.out.println("Dadenot jazol e null");
+//        }
+//    }
+//
+//    public void insertBefore(E o, SLLNode<E> before) {
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            if (first == before) {
+//                this.insertFirst(o);
+//                return;
+//            }
+//            while (tmp.succ != before)
+//                tmp = tmp.succ;
+//            if (tmp.succ == before) {
+//                SLLNode<E> ins = new SLLNode<E>(o, before);
+//                tmp.succ = ins;
+//            } else {
+//                System.out.println("Elementot ne postoi vo listata");
+//            }
+//        } else {
+//            System.out.println("Listata e prazna");
+//        }
+//    }
+//
+//    public void insertLast(E o) {
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            while (tmp.succ != null)
+//                tmp = tmp.succ;
+//            SLLNode<E> ins = new SLLNode<E>(o, null);
+//            tmp.succ = ins;
+//        } else {
+//            insertFirst(o);
+//        }
+//    }
+//
+//    public E deleteFirst() {
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            first = first.succ;
+//            return tmp.element;
+//        } else {
+//            System.out.println("Listata e prazna");
+//            return null;
+//        }
+//    }
+//
+//    public E delete(SLLNode<E> node) {
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            if (first == node) {
+//                return this.deleteFirst();
+//            }
+//            while (tmp.succ != node && tmp.succ.succ != null)
+//                tmp = tmp.succ;
+//            if (tmp.succ == node) {
+//                tmp.succ = tmp.succ.succ;
+//                return node.element;
+//            } else {
+//                System.out.println("Elementot ne postoi vo listata");
+//                return null;
+//            }
+//        } else {
+//            System.out.println("Listata e prazna");
+//            return null;
+//        }
+//    }
+//
+//    public SLLNode<E> getFirst() {
+//        return first;
+//    }
+//
+//    public SLLNode<E> find(E o) {
+//        if (first != null) {
+//            SLLNode<E> tmp = first;
+//            while (tmp.element != o && tmp.succ != null)
+//                tmp = tmp.succ;
+//            if (tmp.element == o) {
+//                return tmp;
+//            } else {
+//                System.out.println("Elementot ne postoi vo listata");
+//            }
+//        } else {
+//            System.out.println("Listata e prazna");
+//        }
+//        return first;
+//    }
+//}
+//
+//class Forum {
+//    // TODO: implement function
+//    private static void forum(SLL<Discussion> health, SLL<Discussion> finance) {
+//        //nasiot kod
+//        /*
+//        input:
+//        3 - br health discuss  3 - br finance discuss
+//        id     pop  users
+//        111    1    1
+//        222    2    4
+//        333    1    1
+//        444    2    7
+//        555    4    6
+//        666    2    9
+//
+//        importance = popularity * users
+//
+//        remove the least important discussion from finance
+//        insert the most important discuss. from health
+//        into finance, it has to have identical
+//        id, pop and users..?
+//        */
+//
+//        SLLNode<Discussion> healthDiscussions = health.getFirst();
+//        SLLNode<Discussion> financeDiscussions = finance.getFirst();
+//        int importance = financeDiscussions.element.importance();
+//        Discussion leastImportant = financeDiscussions.element;
+//
+//        while(financeDiscussions != null){
+//            if(financeDiscussions.element.importance() <
+//                    leastImportant.importance()){
+//                leastImportant = financeDiscussions.element;
+//            }
+//            financeDiscussions = financeDiscussions.succ;
+//        }
+//        finance.delete(finance.find(leastImportant));
+//
+//        Discussion mostImportant = healthDiscussions.element;
+//        while(healthDiscussions != null){
+//            if(healthDiscussions.element.importance() >
+//                    mostImportant.importance()){
+//                mostImportant = healthDiscussions.element;
+//            }
+//            healthDiscussions = healthDiscussions.succ;
+//        }
+//        finance.insertLast(mostImportant);
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int numHealth = Integer.parseInt(scanner.nextLine());
+//        int numFinance = Integer.parseInt(scanner.nextLine());
+//
+//        SLL<Discussion> health = new SLL<Discussion>();
+//        SLL<Discussion> finance = new SLL<Discussion>();
+//
+//        for (int i = 0; i < numHealth; i++) {
+//            String line = scanner.nextLine();
+//            String[] parts = line.split("\\s+");
+//            health.insertLast(new Discussion(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+//        }
+//
+//        for (int i = 0; i < numFinance; i++) {
+//            String line = scanner.nextLine();
+//            String[] parts = line.split("\\s+");
+//            finance.insertLast(new Discussion(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+//        }
+//
+//        forum(health, finance);
+//        System.out.println(health.toString());
+//        System.out.println(finance.toString());
+//    }
+//}
